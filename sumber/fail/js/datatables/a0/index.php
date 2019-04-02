af4359984f83d2a1eca2fd169074839ea8a2269a
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-02 12:56:18 +0800

    tukar pautan include

--- sumber/fail/js/datatables/a0/index.php
+++ sumber/fail/js/datatables/a0/index.php
@@ -1,12 +1,12 @@
 <?php
 #############################################################################################################
 include '../atas-set.php';
-include 'diatas.php';
+include '../diatas.php';
 $tableID = 'myTable';
 //$tableClass = 'display';
 $tableClass = 'table';
 include 'jadual.php';
-include 'dibawah.php';
+include '../dibawah.php';
 #############################################################################################################
 ?>
 <script type="text/javascript">
