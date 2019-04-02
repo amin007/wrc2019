342f1df63a8cf70ca8d941c20dc2943e0a3e20d7
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-02 15:25:13 +0800

    tambah menu logout jika tersesat

--- Aplikasi/Fail/Papar/sesat/index.php
+++ Aplikasi/Fail/Papar/sesat/index.php
@@ -10,3 +10,5 @@ if( isset($url) )
 }	?></pre>
 </div><!-- / class="hero-unit" -->
 </div><!-- / class="container" -->
+
+<a class="btn btn-primary btn-large" href="<?php echo URL ?>ruangtamu/logout">Pergi Lebih Jauh<i class="fa fa-binoculars fa-2x"></i></a>
