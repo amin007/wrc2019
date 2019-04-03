92988f0080ccfe16be468763e6e82da01ddf98a3
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-03 08:50:08 +0800

    cari maksima json boleh dihasilkan

--- Aplikasi/Kelas/Utama/Kawal/cari.php
+++ Aplikasi/Kelas/Utama/Kawal/cari.php
@@ -530,13 +530,13 @@ class Cari extends \Aplikasi\Kitab\Kawal
 		$myTable = dpt_senarai('masco');
 		$medan = '*';
 		$carian[] = $t = null;
-		$susun[0]['max'] = 2530;
+		$susun[0]['max'] = 5500;
 
 		list($kira,$json) =//$this->tanya->cariSql($myTable[1], $medan, $carian, $susun);
 		$this->tanya->cariDataJson($myTable[0], $medan, $carian, $susun);
 		$json = $this->buangdata($json);
 
-		//$t = '"draw":1,"recordsTotal":' . $kira . ',"recordsFiltered":' . $kira . ',';
+		$t = '"draw":1,"recordsTotal":' . $kira . ',"recordsFiltered":' . $kira . ',';
 		echo "{ $t \"data\":$json }";
 	}
 #------------------------------------------------------------------------------------------
