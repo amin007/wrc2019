65c80cfc762688e1bba6e7ac81c9b02c6c087ba5
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-04 19:30:02 +0800

    tukar kedudukan tatasusunan

--- Aplikasi/Kelas/Utama/Kawal/cari.php
+++ Aplikasi/Kelas/Utama/Kawal/cari.php
@@ -434,7 +434,8 @@ class Cari extends \Aplikasi\Kitab\Kawal
 		$medan = 'newss,nama,nossm,operator,kp';
 		$carian[] = array('fix'=>'z%like%','atau'=>'WHERE',
 			'medan'=>'concat_ws(" ",newss,nossm,nama)','apa'=>$cari);
-		$susun['dari'] = 10;
+		//$susun['dari'] = 10;
+		$susun[0]['dari'] = 10;
 
 		$paparKes = //$this->tanya->cariSql($myTable, $medan, $carian, $susun);
 		$this->tanya->cariSemuaData($myTable, $medan, $carian, $susun);
@@ -482,7 +483,8 @@ class Cari extends \Aplikasi\Kitab\Kawal
 		$medan = 'seksyen s,msic,keterangan,msic2000,notakaki';
 		$carian[] = array('fix'=>'z%like%','atau'=>'WHERE',
 			'medan'=>'concat_ws(" ",msic,keterangan,notakaki)','apa'=>$cari);
-		$susun['dari'] = 10;
+		//$susun['dari'] = 10;
+		$susun[0]['dari'] = 10;
 
 		$paparKes = //$this->tanya->cariSql($myTable, $medan, $carian, $susun);
 		$this->tanya->cariSemuaData($myTable, $medan, $carian, $susun);
