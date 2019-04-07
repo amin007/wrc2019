fb2630f7794085a2954ffed1afcf651301e4b06c
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-07 16:16:00 +0800

    susun semula koding bagi carian nampak highlight

--- sumber/fail/js/datatables/a004/msic-cari.php
+++ sumber/fail/js/datatables/a004/msic-cari.php
@@ -1,6 +1,6 @@
 <?php
 #############################################################################################################
-include '../atas-set.php';
+include '../atas-set-001.php';
 include '../diatas.php';
 $tableID = 'myTable';
 $tableClass = 'table table-striped table-bordered';
@@ -12,17 +12,78 @@ include '../dibawah.php';
 #############################################################################################################
 ?>
 <script type="text/javascript">
-$(document).ready(function(){
-///////////////////////////////////////////////////////////
-/*$('#myTable').DataTable({
-	/*"processing": true,
-	"serverSide": true,
-	"ajax": "../server_side/scripts/server_processing.php"
-	"ajax": "../../../../../cari/msicjson"
-});*/
+/////////////////////////////////////////////////////////////
+jQuery.extend({
+	highlight: function (node, re, nodeName, className)
+	{
+		if (node.nodeType === 3)
+		{
+			var match = node.data.match(re);
+			if (match)
+			{
+				var highlight = document.createElement(nodeName || 'span');
+				highlight.className = className || 'highlight';
+				var wordNode = node.splitText(match.index);
+				wordNode.splitText(match[0].length);
+				var wordClone = wordNode.cloneNode(true);
+				highlight.appendChild(wordClone);
+				wordNode.parentNode.replaceChild(highlight, wordNode);
+				return 1; //skip added node in parent
+			}
+		}
+		else if ((node.nodeType === 1 && node.childNodes) && //only element nodes that have children
+			!/(script|style)/i.test(node.tagName) && //ignore script and style nodes
+			!(node.tagName === nodeName.toUpperCase() && node.className === className))
+		{//skip if already highlighted
+			for (var i = 0; i < node.childNodes.length; i++)
+			{
+				i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
+			}
+		}
+		return 0;
+	}
+});
+
+jQuery.fn.unhighlight = function (options)
+{
+	var settings = { className: 'highlight', element: 'span' };
+	jQuery.extend(settings, options);
+
+	return this.find(settings.element + "." + settings.className).each(function ()
+	{
+		var parent = this.parentNode;
+		parent.replaceChild(this.firstChild, this);
+		parent.normalize();
+	}).end();
+};
+
+jQuery.fn.highlight = function (words, options)
+{
+	var settings = { className: 'highlight', element: 'span', caseSensitive: false, wordsOnly: false };
+	jQuery.extend(settings, options);
+
+	if (words.constructor === String){words = [words];}
+	words = jQuery.grep(words, function(word, i){return word != '';});
+	words = jQuery.map(words, function(word, i)
+	{
+		return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+	});
+	if (words.length == 0) { return this; };
+
+	var flag = settings.caseSensitive ? "" : "i";
+	var pattern = "(" + words.join("|") + ")";
+	if (settings.wordsOnly){pattern = "\\b" + pattern + "\\b";}
+	var re = new RegExp(pattern, flag);
+
+	return this.each(function ()
+	{
+		jQuery.highlight(this, re, settings.element, settings.className);
+    });
+};
 /////////////////////////////////////////////////////////////
 	var t = $('#myTable').DataTable({
 	"ajax": "../../../../../cari/msicjson",
+	searchHighlight: true,
 	"columnDefs": [{
 		"searchable": false,
 		"orderable": false,
@@ -35,9 +96,7 @@ $(document).ready(function(){
 		t.column(0, {search:'applied', order:'applied'}).nodes().
 		each( function (cell, i) {cell.innerHTML = i+1;});
     }).draw();
-	searchHighlight: true;
 /////////////////////////////////////////////////////////////
-});
 </script>
 </body>
 </html><?php
