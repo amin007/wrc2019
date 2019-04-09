17d1cebcdb81b622da10f04d1f00b0fb11440a7e
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-09 12:02:32 +0800

    tambah set tatarajah

---
+++ sumber/fail/php/poslaju/set-tatarajah.php
@@ -0,0 +1,20 @@
+<?php
+$baris[] = 'AMIN LEDANG0';
+$baris[] = 'NO 1';
+$baris[] = 'JALAN 2, TANJUNG AGAS';
+$baris[] = 'MUAR';
+$baris[] = 'JOHOR';
+$baris[] = 'TEL:0123456789';
+$baris[] = 'GOOGLE MALAYSIA&nbsp;&nbsp;(SURAT)';
+$baris[] = 'Google Malaysia
+Level 20, Axiata Tower, 9,
+Jalan Stesen Sentral 5,
+Kuala Lumpur Sentral,
+50470 Kuala Lumpur
+';
+$baris[] = 'KUALA LUMPUR';
+$baris[] = 'KUALA LUMPUR';
+$baris[] = 'TEL:03-2261 6400';
+$baris[] = '50470';
+$baris[] = 'SURAT';
+$baris[] = '';
