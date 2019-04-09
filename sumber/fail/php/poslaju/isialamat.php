609b60a03f801d45df743cc23569834487a07bbf
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-04-09 12:11:05 +0800

    skrip asal

---
+++ sumber/fail/php/poslaju/isialamat.php
@@ -0,0 +1,127 @@
+
+
+
+<BODY><DIV id="print_area">
+
+<DIV id="wb_Text2" style="left: 264px; top: 10px; width: 250px; height: 12px; text-align: left; position: absolute;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 15px;">-</SPAN></DIV>
+
+<DIV id="wb_Text2" style="left: 70px; top: 57px; width: 250px; height: 16px; text-align: left; position: absolute; z-index: 0;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 15px;">AMIN LEDANG</SPAN></DIV>
+<DIV id="wb_Text3" style="left: 70px; top: 79px; width: 250px; height: 15px; text-align: left; position: absolute; z-index: 1;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">NO 1</SPAN></DIV>
+<DIV id="wb_Text4" style="left: 70px; top: 90px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 2;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">JALAN 2, TANJUNG AGAS</SPAN></DIV>
+<DIV id="wb_Text5" style="left: 70px; top: 107px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 3;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">MUAR</SPAN></DIV>
+<DIV id="wb_Text6" style="left: 70px; top: 121px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 4;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">JOHOR</SPAN></DIV>
+
+<DIV id="wb_Text7" style="left: 70px; top: 135px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 5;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">TEL:0123456789</SPAN></DIV>
+
+<DIV id="wb_Text8" style="left: 70px; top: 200px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 6;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">GOOGLE MALAYSIA&nbsp;&nbsp;(SURAT)</SPAN></DIV>
+
+<DIV id="wb_Text9" style="left: 70px; top: 219px; width: 300px; height: 39px; text-align: left; position: absolute; z-index: 7;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">Google Malaysia
+Level 20, Axiata Tower, 9,
+Jalan Stesen Sentral 5,
+Kuala Lumpur Sentral,
+50470 Kuala Lumpur
+</SPAN></DIV>
+
+<DIV id="wb_Text12" style="left: 70px; top: 264.5px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 9;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">KUALA LUMPUR</SPAN></DIV>
+
+<DIV id="wb_Text14" style="left: 70px; top: 279.7px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 10;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">KUALA LUMPUR</SPAN></DIV>
+
+<DIV id="wb_Text11" style="left: 70px; top: 295px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 11;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">TEL:03-2261 6400</SPAN></DIV>
+
+<DIV id="wb_Text13" style="left: 329px; top: 330px; width: 250px; height: 38px; text-align: left; position: absolute; z-index: 12;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 26px;">50470</SPAN></DIV>
+
+<DIV id="wb_Text13" style="left: 406px; top: 139.84px; width: 250px; height: 13px; text-align: left; position: absolute; z-index: 13;"><SPAN
+style="color: rgb(0, 0, 0); font-family: Arial; font-size: 13px;">SURAT </SPAN></DIV>
+
+</DIV></BODY><form method="post" action="/index.php">
+
+Data has been generated to be printed.<title>Print Poslaju Slip Form</title>
+<head>
+<meta property="og:url"                content="http://poslaju.wujiecourier.com" />
+<meta property="og:type"               content="website" />
+<meta property="og:title"              content="Online Printing Poslaju Slip Form" />
+<meta property="og:description"        content="Online Printing Poslaju Slip Form with EPSON LQ-310" />
+<meta property="og:image"              content="http://poslaju.wujiecourie.com/poslaju_slip_form_sample.png" />
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '439665946376269'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=439665946376269&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+</head>
+<center><u>Free Printable Template - Poslaju Form</u><div class="fb-like" data-href="http://poslaju.wujiecourie.com" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div></center>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<br>
+<table >
+<tr><td>Nama Pengirim:</td><td><span class="error">*</span></td><td><input type="text" name="nama_pengirim" size="35"></td>
+<td rowspan="17" align="center"><img src="poslaju_slip_form_sample.png" alt="Poslaju Slip Form Sample" style="width:609px;height:290px;">
+<br><a href="http://poslaju.wujiecourier.com">New Poslaju Slip Form (Recommended)</a>
+<br><iframe width="560" height="315" src="https://www.youtube.com/embed/g2ZwgvfT8tM" frameborder="0" allowfullscreen></iframe></td></tr>
+<tr><td>Alamat Pengirim Baris 1:</td><td><span class="error">*</span></td><td><input type="text" size="35"name="alamat_pengirim_1"></td></tr>
+<tr><td>Alamat Pengirim Baris 2:</td><td><span class="error">*</span></td><td><input type="text" size="35"name="alamat_pengirim_2"></td></tr>
+<tr><td>Bandar Pengirim: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="bandar_pengirim"></td></tr>
+<tr><td>Tel. Pengirim:</td><td><span class="error">*</span></td><td><input type="text" size="35"name="tel_pengirim"></td></tr>
+<tr><td>Negeri Pengirim: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="negeri_pengirim"></td></tr>
+<tr><td colspan="3" height="14px"></td></tr>
+<tr><td>Nama Penerima: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="nama_penerima"></td></tr>
+<tr><td>Product: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="product"></td></tr>
+<tr><td>Seller:</td><td><span class="error">*</span></td><td><input type="text" size="35"name="seller"></td></tr>
+
+<tr valign="top"><td>Alamat Penerima: </td><td><span class="error">*</span></td><td><textarea name="alamat_penerima_baris_1" rows="5" cols="25"></textarea></td></tr>
+<tr><td>Poskod Penerima: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="poskod_penerima"></td></tr>
+<tr><td>Bandar Penerima: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="bandar_penerima"></td></tr>
+<tr><td>Tel. Penerima: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="tel_penerima"></td></tr>
+<tr><td>Negeri Penerima: </td><td><span class="error">*</span></td><td><input type="text" size="35"name="negeri_penerima"></td></tr>
+<tr><td colspan="3" height="14px"></td></tr>
+<tr><td colspan="3"><input type="submit" name="submit" value="Submit"> </td></tr>
+</table>
+</form>
+
+
+<script type="text/javascript">
+(function PrintDiv() {
+	var divToPrint = document.getElementById('print_area');
+	var html = divToPrint.innerHTML;
+	var popupWin = window.open('', 'PRINT', 'height=400,width=600');
+	divToPrint.style.visibility="hidden";
+	console.log('pw', popupWin);
+	console.log('dtp', divToPrint);
+	popupWin.document.open();
+	popupWin.document.write('<html><title>Print Slip Form Poslaju</title><body onload="window.print()">' + html + '</html>');
+	popupWin.document.close();
+	//popupWin.focus();
+	//popupWin.print();
+	popupWin.close();
+	return true;
+})();
+</script>
