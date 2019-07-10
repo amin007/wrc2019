9fdeb63479bdf83a8705e0d9dc0d364763c7487f
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-10 17:10:54 +0800

    susun tatasusunan

--- Aplikasi/Fail/Papar/kiraan/komponen-io.php
+++ Aplikasi/Fail/Papar/kiraan/komponen-io.php
@@ -3,10 +3,9 @@
 $kiraF = array('01','02','03','04','05','06','07','11','12');
 ?>
 <script>
-$('#F1318xx').keyup(function(){
+$('#F1318xa').keyup(function(){
 <?php foreach ($kiraF as $ff):?>
-	var F1318<?php echo $ff ?>;
-	F1318<?php echo $ff ?> = parseFloat($('#F1318<?php echo $ff ?>').val());
+	var F1318<?php echo $ff ?>; F1318<?php echo $ff ?> = parseFloat($('#F1318<?php echo $ff ?>').val());
 <?php endforeach; ?>
 	var F880499; F880499 = parseFloat($('#F880499').val());
 	var F141801; F141801 = parseFloat($('#141801').val());
@@ -23,10 +22,9 @@ $kiraF = array('02','03','04','05','06','07',
 '18','23','25','30','32','33','37','50','51','52');
 ?>
 <script>
-$('#F1418xx').keyup(function(){
+$('#F1418xb').keyup(function(){
 <?php foreach ($kiraF as $ff):?>
-	var F1418<?php echo $ff ?>;
-	F1418<?php echo $ff ?> = parseFloat($('#F1418<?php echo $ff ?>').val());
+	var F1418<?php echo $ff ?>; F1418<?php echo $ff ?> = parseFloat($('#F1418<?php echo $ff ?>').val());
 <?php endforeach; ?>
 //	var result = F00<?php echo $ff ?>a + F00<?php echo $ff ?>b + F00<?php echo $ff ?>c;
 //	$('#F00<?php echo $ff ?>').val(result.toFixed(0));
