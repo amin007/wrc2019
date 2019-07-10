a7a36099c189e29ea685fddf1b1c8ae0d99a3e6a
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-10 18:10:37 +0800

    tambha kiraan IO

--- Aplikasi/Fail/Papar/kiraan/b_kira.php
+++ Aplikasi/Fail/Papar/kiraan/b_kira.php
@@ -50,13 +50,13 @@ $labelF = array('Kos pembaikan','Penbelian bahan & bekalan','kos pencetakan',
 'Kos perkerjaan:bayaran kpd pengarah tak bekerja',
 'Kos perkerjaan:nilai pakaian percuma',
 'Kos perkerjaan:kos latihan kpd pekerja',
-'Input');
+'Input','IO');
 $f0 = 'F1417';
 $kiraF0 = array($f0.'02',$f0.'03',$f0.'04',$f0.'05',$f0.'06',$f0.'07',
-$f0.'18',$f0.'23',$f0.'25',$f0.'30',$f0.'32',$f0.'33',$f0.'37',$f0.'50',$f0.'51',$f0.'52','F1417xb');
+$f0.'18',$f0.'23',$f0.'25',$f0.'30',$f0.'32',$f0.'33',$f0.'37',$f0.'50',$f0.'51',$f0.'52','F1417xb','IO2017');
 $f1 = 'F1418';
 $kiraF1 = array($f1.'02',$f1.'03',$f1.'04',$f1.'05',$f1.'06',$f1.'07',
-$f1.'18',$f1.'23',$f1.'25',$f1.'30',$f1.'32',$f1.'33',$f1.'37',$f1.'50',$f1.'51',$f1.'52','F1418xb');
+$f1.'18',$f1.'23',$f1.'25',$f1.'30',$f1.'32',$f1.'33',$f1.'37',$f1.'50',$f1.'51',$f1.'52','F1418xb','IO2018');
 ?>
 <?php foreach ($labelF as $key => $ff):?>
 <div class="form-row">
