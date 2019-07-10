cbd466478fc37e3fc9e7f20bfa1ac6bc4a072038
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-10 19:11:18 +0800

    contoh penuh html dalam kiraan

---
+++ sumber/fail/php/convert/kiraan.php
@@ -0,0 +1,280 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>BANCI 2019</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet" type="text/css">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<style type="text/css">
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+.fa-input {font-family: FontAwesome}
+</style>
+</head>
+<body><!-- menu_atas.php A4.1.3 -->
+
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+	<a class="navbar-brand" href="http://jpmuar07-pc/projek-rahsia/tahun/wrc2019/">
+		<i class="fas fa-video"></i>BANCI 2019:</a>
+	<a class="navbar-brand" href="http://jpmuar07-pc/projek-rahsia/tahun/wrc2019/ruangtamu/logout">
+		<i class="fa fa-times fa-2x" aria-hidden="true"></i>Keluar</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+	aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+</nav>
+<br>
+
+<!-- menu tengah atas -->
+<div class="container">
+<h1>Komponen Output</h1>
+<form>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">1|Jualan barang2</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131701" placeholder="F131701"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131801" value="5" placeholder="F131801"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">2|Jualan kenderaan bermotor</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131702" placeholder="F131702"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131802" value="5" placeholder="F131802"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">3|Jualan alat ganti & aksesori</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131703" placeholder="F131703"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131803" value="5" placeholder="F131803"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">4|Jualan petrol dan gas</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131704" placeholder="F131704"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131804" value="5" placeholder="F131804"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">5|Komisen, brokeraj dan yuran yang diterima</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131705" placeholder="F131705"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131805" value="5" placeholder="F131805"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">6|Pdptn drpd perkhidmatan pembaikan,pemasangan & penyelenggaraan</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131706" placeholder="F131706"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131806" value="5" placeholder="F131806"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">7|Royalti, hakcipta,pelesenan & yuran francais</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131707" placeholder="F131707"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131807" value="5" placeholder="F131807"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">8|Pdptn drpd sewa Lain-lain</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131711" placeholder="F131711"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131811" value="5" placeholder="F131811"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">9|Pdptn operasi lain pindahan wang</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131712" placeholder="F131712"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F131812" value="5" placeholder="F131812"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">10|Jum membuat/membina sendiri (soalan aset)</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F870499" placeholder="F870499"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F880499" value="5" placeholder="F880499"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">11|Kos barangan yg dijual</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141701" placeholder="F141701"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141801" value="5" placeholder="F141801"></div>
+</div>
+</form>
+<hr>
+<h1>Komponen Input</h1>
+<form>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">1|Kos pembaikan</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141702" placeholder="F141702"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141802" placeholder="F141802"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">2|Penbelian bahan & bekalan</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141703" placeholder="F141703"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141803" placeholder="F141803"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">3|kos pencetakan</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141704" placeholder="F141704"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141804" placeholder="F141804"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">4|Air yg dibeli</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141705" placeholder="F141705"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141805" placeholder="F141805"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">5|Tenaga elektrik yg dibeli</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141706" placeholder="F141706"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141806" placeholder="F141806"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">6|Bahan pembakar,pelincir & gas</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141707" placeholder="F141707"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141807" placeholder="F141807"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">7|Pembelian perkhidmatan pengangkutan</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141718" placeholder="F141718"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141818" placeholder="F141818"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">8|Bayaran telekomunikasi (cth telefon,internet)</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141723" placeholder="F141723"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141823" placeholder="F141823"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">9|Pengiklanan & promosi</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141725" placeholder="F141725"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141825" placeholder="F141825"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">10|Bayaran sewa Sewaan operasi lain</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141730" placeholder="F141730"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141830" placeholder="F141830"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">11|Bayaran royalti kerajaan/badan berkanun</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141732" placeholder="F141732"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141832" placeholder="F141832"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">12|Bayaran royalti ngo/tajaan korporat</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141733" placeholder="F141733"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141833" placeholder="F141833"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">13|Perbelanjaan operasi lain</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141737" placeholder="F141737"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141837" placeholder="F141837"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">14|Kos perkerjaan:bayaran kpd pengarah tak bekerja</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141750" placeholder="F141750"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141850" placeholder="F141850"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">15|Kos perkerjaan:nilai pakaian percuma</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141751" placeholder="F141751"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141851" placeholder="F141851"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">16|Kos perkerjaan:kos latihan kpd pekerja</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141752" placeholder="F141752"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F141852" placeholder="F141852"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">17|Input</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F1417xb" placeholder="F1417xb"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F1418xb" placeholder="F1418xb" readonly></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">18|Output</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F1317xa" placeholder="F1317xa"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="F1318xa" placeholder="F1318xa"></div>
+</div>
+<div class="form-row">
+	<div class="form-group col-md-5"><label for="inputZip">19|IO</label></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="IO2017" placeholder="IO2017"></div>
+	<div class="form-group col-md-3"><input type="text" class="form-control form-control-sm" id="IO2018" placeholder="IO2018"></div>
+</div>
+</form>
+</div><!-- / class="container" -->
+
+<!-- menu tengah bawah -->
+<!-- Footer
+================================================== -->
+<!-- footer class="footer">
+	<div class="container">
+		<span class="label label-info">
+		&copy; Hak Cipta Terperihara 2019. Theme Asal Bootstrap Twitter</span>
+	</div>
+</footer -->
+
+<!-- khas untuk jquery dan js2 lain
+================================================== -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
+<script>
+$('#F1318xa').keyup(function(){
+	var F131801 = parseFloat($('#F131801').val()) || 0;
+	var F131802 = parseFloat($('#F131802').val()) || 0;
+	var F131803 = parseFloat($('#F131803').val()) || 0;
+	var F131804 = parseFloat($('#F131804').val()) || 0;
+	var F131805 = parseFloat($('#F131805').val()) || 0;
+	var F131806 = parseFloat($('#F131806').val()) || 0;
+	var F131807 = parseFloat($('#F131807').val()) || 0;
+	var F131811 = parseFloat($('#F131811').val()) || 0;
+	var F131812 = parseFloat($('#F131812').val()) || 0;
+	var F880499; F880499 = parseFloat($('#F880499').val()) || 0;
+	var F141801; F141801 = parseFloat($('#F141801').val()) || 0;
+
+	var resultT = F131801 + F131802 + F131803 + F131804 + F131805 + F131806 + F131807
+		+ F131811 + F131812 + F880499;
+	var result = resultT - F141801;
+	$('#F1318xa').val(result.toFixed(0));
+});
+
+$('#F1418xb').keyup(function(){
+	var F141802 = parseFloat($('#F141802').val()) || 0;
+	var F141803 = parseFloat($('#F141803').val()) || 0;
+	var F141804 = parseFloat($('#F141804').val()) || 0;
+	var F141805 = parseFloat($('#F141805').val()) || 0;
+	var F141806 = parseFloat($('#F141806').val()) || 0;
+	var F141807 = parseFloat($('#F141807').val()) || 0;
+	var F141818 = parseFloat($('#F141818').val()) || 0;
+	var F141823 = parseFloat($('#F141823').val()) || 0;
+	var F141825 = parseFloat($('#F141825').val()) || 0;
+	var F141830 = parseFloat($('#F141830').val()) || 0;
+	var F141832 = parseFloat($('#F141832').val()) || 0;
+	var F141833 = parseFloat($('#F141833').val()) || 0;
+	var F141837 = parseFloat($('#F141837').val()) || 0;
+	var F141850 = parseFloat($('#F141850').val()) || 0;
+	var F141851 = parseFloat($('#F141851').val()) || 0;
+	var F141852 = parseFloat($('#F141852').val()) || 0;
+	var result = F141802 + F141803 + F141804 + F141805 + F141806 + F141807 + F141818 + F141823 + F141825
+	+ F141830 + F141832 + F141833 + F141837 + F141850 + F141851 + F141852;
+	$('#F1418xb').val(result.toFixed(0));
+});
+
+$('#IO2018').keyup(function(){
+	var F1318xa = parseFloat($('#F1318xa').val()) || 0;
+	var F1418xb = parseFloat($('#F1418xb').val()) || 0;
+	var io = (F1318xa / F1418xb);
+	$('#IO2018').val(io.toFixed(2));
+});
+</script>
+
+</body>
+</html>
