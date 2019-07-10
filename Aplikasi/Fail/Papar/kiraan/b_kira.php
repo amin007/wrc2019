4698f9c69de84caf85f605982fcbf83e836b97a4
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-10 19:00:54 +0800

    tukar susunan pembolehubah dan tatasusunan input output

--- Aplikasi/Fail/Papar/kiraan/b_kira.php
+++ Aplikasi/Fail/Papar/kiraan/b_kira.php
@@ -52,10 +52,10 @@ $labelF = array('Kos pembaikan','Penbelian bahan & bekalan','kos pencetakan',
 'Input','Output','IO');
 $f0 = 'F1417';
 $kiraF0 = array($f0.'02',$f0.'03',$f0.'04',$f0.'05',$f0.'06',$f0.'07',
-$f0.'18',$f0.'23',$f0.'25',$f0.'30',$f0.'32',$f0.'33',$f0.'37',$f0.'50',$f0.'51',$f0.'52','F1317xa','F1417xb','IO2017');
+$f0.'18',$f0.'23',$f0.'25',$f0.'30',$f0.'32',$f0.'33',$f0.'37',$f0.'50',$f0.'51',$f0.'52','F1417xb','F1317xa','IO2017');
 $f1 = 'F1418';
 $kiraF1 = array($f1.'02',$f1.'03',$f1.'04',$f1.'05',$f1.'06',$f1.'07',
-$f1.'18',$f1.'23',$f1.'25',$f1.'30',$f1.'32',$f1.'33',$f1.'37',$f1.'50',$f1.'51',$f1.'52','F1318xa','F1418xb','IO2018');
+$f1.'18',$f1.'23',$f1.'25',$f1.'30',$f1.'32',$f1.'33',$f1.'37',$f1.'50',$f1.'51',$f1.'52','F1418xb','F1318xa','IO2018');
 ?>
 <?php foreach ($labelF as $key => $ff):?>
 <div class="form-row">
@@ -64,11 +64,11 @@ $f1.'18',$f1.'23',$f1.'25',$f1.'30',$f1.'32',$f1.'33',$f1.'37',$f1.'50',$f1.'51'
 	</div>
 	<div class="form-group col-md-3">
 		<input type="text" class="form-control form-control-sm" id="<?php echo $kiraF0[$key] ?>" placeholder="<?php echo $kiraF0[$key] ?>"<?php
-		echo ( $kiraF0[$key] == 'F1317xa') ? ' readonly':''?>>
+		echo ( $kiraF0[$key] == 'F1418xb') ? ' readonly':''?>>
 	</div>
 	<div class="form-group col-md-3">
 		<input type="text" class="form-control form-control-sm" id="<?php echo $kiraF1[$key] ?>" placeholder="<?php echo $kiraF1[$key] ?>"<?php
-		echo ( $kiraF1[$key] == 'F1318xa') ? ' readonly':''?>>
+		echo ( $kiraF1[$key] == 'F1418xb') ? ' readonly':''?>>
 	</div>
 </div>
 <?php endforeach; ?>
