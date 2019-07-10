c13239ca0e2e1505602c85185f7f153825f99905
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-10 17:18:56 +0800

    buat folmula

--- Aplikasi/Fail/Papar/kiraan/komponen-io.php
+++ Aplikasi/Fail/Papar/kiraan/komponen-io.php
@@ -11,7 +11,10 @@ $('#F1318xa').keyup(function(){
 	var F141801; F141801 = parseFloat($('#141801').val());
 
 //	var result = F00<?php echo $ff ?>a + F00<?php echo $ff ?>b + F00<?php echo $ff ?>c;
-//	$('#F00<?php echo $ff ?>').val(result.toFixed(0));
+	var resultT = F131801 + F131802 + F131803 + F131804 + F131805 + F131806 + F131807
+	+ F131811 + F131812 + F880499;
+	var result = resultT - F141801;
+	$('#F00<?php echo $ff ?>').val(result.toFixed(0));
 });
 </script>
 
