edeff520e934eacb0af1380ae8530370ddd0ecbd
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-22 12:07:40 +0800

    tambah form-control-lg

--- sumber/fail/php/template/bootstrap/4.3.1/borangLogin001.php
+++ sumber/fail/php/template/bootstrap/4.3.1/borangLogin001.php
@@ -1,12 +1,12 @@
 <br><form>
 	<div class="form-group">
 		<label for="exampleInputEmail1">Email</label>
-		<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+		<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
 		<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 	</div>
 	<div class="form-group">
 		<label for="exampleInputPassword1">Rahsia Antara Kita</label>
-		<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		<input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
 	</div>
 	<div class="form-group form-check">
 		<input type="checkbox" class="form-check-input" id="exampleCheck1">
