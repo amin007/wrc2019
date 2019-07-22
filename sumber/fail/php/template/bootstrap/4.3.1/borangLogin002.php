f77e5bce5e4658f9e481b4f97f0d0d696d632757
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-22 15:52:29 +0800

    tambah class khas untuk label

--- sumber/fail/php/template/bootstrap/4.3.1/borangLogin002.php
+++ sumber/fail/php/template/bootstrap/4.3.1/borangLogin002.php
@@ -1,10 +1,10 @@
 <form>
 	<div class="form-group">
-		<div class="bg-secondary text-white"><label for="contohNama">Nama</label></div>
+		<div class="bg-secondary text-white"><label class="kotakLabel" for="contohNama">Nama<label></div>
 		<input type="email" class="form-control" placeholder="Masukkan Nama Gelaran">
-		<div class="bg-secondary text-white"><label for="contohEmail">Email</label></div>
+		<div class="bg-secondary text-white"><label class="kotakLabel" for="contohEmail">Email</label></div>
 		<input type="email" class="form-control" placeholder="Masukkan Email">
-		<div class="bg-secondary text-white"><label for="contohPassword">Rahsia Antara Kita</label></div>
+		<div class="bg-secondary text-white"><label class="kotakLabel" for="contohPassword">Rahsia Antara Kita</label></div>
 		<input type="password" class="form-control" placeholder="Password">
 		<input type="submit" class="btn btn-primary btn-block" value="Simpan">
 	</div><!-- / class="form-group" -->
