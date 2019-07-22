74b35e326a12f74c77ae8a4f1181b909bd54780f
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-22 12:59:50 +0800

    tukar tajuk

--- sumber/fail/php/template/bootstrap/diatas.php
+++ sumber/fail/php/template/bootstrap/diatas.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
-<title>List Folder</title>
+<title>Contoh Borang Login</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" rel="stylesheet" type="text/css">
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
