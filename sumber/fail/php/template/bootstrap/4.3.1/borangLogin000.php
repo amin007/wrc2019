78a0ddf0c1a1cdfb4e5790362318e68fb3fdb8f6
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-22 11:27:30 +0800

    masukkan contoh borang berasaskan boottstrap

---
+++ sumber/fail/php/template/bootstrap/4.3.1/borangLogin000.php
@@ -0,0 +1,11 @@
+<br><form>
+	<div class="form-group">
+		<label for="a">Selamat datang ke halaman kami</label>
+	</div>
+	<div class="form-group">
+		<label for="b">Macam mana kehidupan anda pada hari ini</label>
+	</div>
+	<div class="form-group">
+		<label for="c">Semua anda ceria sepanjang masa</label>
+	</div>
+</form>
