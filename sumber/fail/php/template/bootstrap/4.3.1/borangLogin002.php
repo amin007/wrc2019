02a532cf518b291d8899ce7c8924a5a241c23c2d
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-22 11:40:17 +0800

    tambah borang daftar

---
+++ sumber/fail/php/template/bootstrap/4.3.1/borangLogin002.php
@@ -0,0 +1,17 @@
+<br><form>
+	<div class="form-group">
+		<label for="contohNama">Nama</label>
+		<input type="email" class="form-control" placeholder="Siapa Nama">
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<label for="contohEmail">Email</label>
+		<input type="email" class="form-control" placeholder="Masukkan Email">
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<label for="contohPassword">Rahsia Antara Kita</label>
+		<input type="password" class="form-control" placeholder="Password">
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<input type="submit" class="btn btn-primary btn-block" value="Simpan">
+	</div><!-- / class="form-group" -->
+</form>
