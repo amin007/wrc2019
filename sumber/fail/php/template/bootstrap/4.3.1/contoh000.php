d29ff6df0c8b1065568002d42071f5b68a679d4d
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-22 11:29:16 +0800

    masukkan contoh template

--- sumber/fail/php/template/bootstrap/4.3.1/contoh000.php
+++ sumber/fail/php/template/bootstrap/4.3.1/contoh000.php
@@ -7,15 +7,23 @@
 		<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+		<a class="nav-link" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="false">Login</a>
 	</li>
 	<li class="nav-item">
 		<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
 	</li>
 	</ul>
 	<div class="tab-content" id="myTabContent">
-		<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...Home</div>
-		<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...Profile</div>
+		<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<?php include 'borangLogin000.php'; ?>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+		<div class="tab-pane fade" id="login" role="tabpanel" aria-labelledby="login-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<?php include 'borangLogin001.php'; ?>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
 		<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...Contact</div>
 	</div>
 </div><!-- / class="kotakTengah" -->
