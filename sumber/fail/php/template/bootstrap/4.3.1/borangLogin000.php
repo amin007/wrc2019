3093444523adaa66eadbf6d8b0cfc2b70cae2783
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-07-22 12:19:17 +0800

    tambah warna pada teks

--- sumber/fail/php/template/bootstrap/4.3.1/borangLogin000.php
+++ sumber/fail/php/template/bootstrap/4.3.1/borangLogin000.php
@@ -1,11 +1,11 @@
 <form>
 	<div class="form-group">
-		<label for="a">Selamat datang ke halaman kami</label>
+		<label for="a" class="text-info">Selamat datang ke halaman kami</label>
 	</div>
 	<div class="form-group">
-		<label for="b">Macam mana kehidupan anda pada hari ini</label>
+		<label for="b" class="text-warning">Macam mana kehidupan anda pada hari ini</label>
 	</div>
 	<div class="form-group">
-		<label for="c">Semua anda ceria sepanjang masa</label>
+		<label for="c" class="text-success">Semua anda ceria sepanjang masa</label>
 	</div>
 </form>
