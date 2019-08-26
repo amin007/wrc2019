3aecb41d085fa1ccf19e83797ad1431efc994dab
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-26 19:35:33 +0800

    tambah form carian

--- sumber/fail/js/datatables/b000/syarikat-cari.php
+++ sumber/fail/js/datatables/b000/syarikat-cari.php
@@ -4,13 +4,39 @@ include '../atas-set-001.php';
 include '../diatas.php';
 #############################################################################################################
 #------------------------------------------------------------------------------------------------------------
-?><h1>Papar Syarikat</h1><hr>
+$carian = null;
+if ($carian=='[id:0]')
+{
+	echo 'data kosong<br>';
+}
+else
+{// $this->carian=='newss' - mula
+	//echo $halaman . '<br>';
+?>
+<h1></h1>
+<div class="container">
+<form method="GET" action="<?=$mencari?>" class="form-inline" autocomplete="off">
+<div class="form-group"><div class="input-group">
+	<div class="input-group-append"><span class="input-group-text">
+		Papar Id Syarikat
+	</span></div>
+	<input type="text" name="cari" class="form-control" autofocus
+	id="inputString" onkeyup="lookup(this.value);" onblur="fill();">
+	<div class="input-group-append">
+		<input type="submit" value="Kemaskini" class="btn btn-outline-secondary">
+	</div>
+</div></div>
+<div class="suggestionsBox" id="suggestions" style="display: none; " >
+	<div class="suggestionList" id="autoSuggestionsList">&nbsp;</div>
+</div>
+</form></div><hr>
 <div class="container">
 	<div class="row" id="csv-display" style="height:500px;overflow: scroll;">
 	</div>
 </div>
 <hr>
 <?php
+}
 #------------------------------------------------------------------------------------------------------------
 #############################################################################################################
 include '../dibawah.php';
