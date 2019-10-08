c41c4c6bd6614c3da1e57ad9ab7d1800ce19b5c9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-10-08 15:54:35 +0800

    tambah fungsi baru

--- Aplikasi/Kelas/Utama/Kawal/cari.php
+++ Aplikasi/Kelas/Utama/Kawal/cari.php
@@ -662,5 +662,21 @@ class Cari extends \Aplikasi\Kitab\Kawal
 		$this->paparJsondaa($json,$kira,$t);
 	}
 #------------------------------------------------------------------------------------------
+	public function institutjson()
+	{
+		//echo '<hr> Nama class : ' . __METHOD__ . '<hr>';
+		$myTable = dpt_senarai('jadual_json');
+		$medan = '"" as no,kod,keterangan,nota01';
+		$carian[] = $t = null;
+		$susun[0]['max'] = 2000;//702-5500
+		//$susun[0]['susun'] = '';
+
+		list($kira,$json) =//$this->tanya->cariSql($myTable[5], $medan, $carian, $susun);
+		$this->tanya->cariDataJson($myTable[5], $medan, $carian, $susun);
+		//$this->tanya->cariDataJson2($myTable[5], $medan, $carian, $susun);
+
+		$this->paparJsondaa($json,$kira,$t);
+	}
+#------------------------------------------------------------------------------------------
 #==========================================================================================
 }
