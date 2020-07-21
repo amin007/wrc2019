dace3b05c982bf31350f96c148471a521f87aee7
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-07-21 16:12:57 +0800

    contoh daa

--- i-sql001.php
+++ i-sql001.php
@@ -1,13 +1,41 @@
 <?php
+//https://gist.github.com/arsho/193141aa91ea7ee34119aa4d6ccadcd9
 	include 'tatarajah.php';
 	$dsn = DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8';
 	$conn = new PDO($dsn, DB_USER, DB_PASS);
+#--------------------------------------------------------------------------------------------------
+/*	$sql_query = "SELECT * FROM wrong_user_table";
+	$stmt = $conn->prepare($sql_query);
+	$stmt->execute();
+	$result_set = $stmt->fetchAll();
+	var_dump($result_set);
+	# Get the current error mode of PDO
+	$current_error_mode = $conn->getAttribute(PDO::ATTR_ERRMODE);
+	echo "<br>";
+	echo "Value of PDO::ATTR_ERRMODE: ".$current_error_mode;
+*/
+#--------------------------------------------------------------------------------------------------
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql_query = "SELECT * FROM wrong_user_table";
 	$stmt = $conn->prepare($sql_query);
 	$stmt->execute();
 	$result_set = $stmt->fetchAll();
 	var_dump($result_set);
-	/*Get the current error mode of PDO*/
+	# Get the current error mode of PDO
 	$current_error_mode = $conn->getAttribute(PDO::ATTR_ERRMODE);
 	echo "<br>";
 	echo "Value of PDO::ATTR_ERRMODE: ".$current_error_mode;
+//*/
+#--------------------------------------------------------------------------------------------------
+/*	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	$sql_query = "SELECT * FROM wrong_user_table";
+	$stmt = $conn->prepare($sql_query);
+	$stmt->execute();
+	$result_set = $stmt->fetchAll();
+	var_dump($result_set);
+	# Get the current error mode of PDO
+	$current_error_mode = $conn->getAttribute(PDO::ATTR_ERRMODE);
+	echo "<br>";
+	echo "Value of PDO::ATTR_ERRMODE: ".$current_error_mode;
+//*/
+#--------------------------------------------------------------------------------------------------
